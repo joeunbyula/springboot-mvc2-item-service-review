a49fb3dbff1de1a0d32974e3ff34464da02d4293
joeunbyula <gkdldy1109@gmail.com>
2023-11-06 16:12:53 +0900

    메시지,국제화

--- src/main/java/hello/itemservice/TestDataInit.java
+++ src/main/java/hello/itemservice/TestDataInit.java
@@ -2,6 +2,8 @@ package hello.itemservice;
 
 import hello.itemservice.domain.formItem.FormItem;
 import hello.itemservice.domain.formItem.FormItemRepository;
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,15 +13,18 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 public class TestDataInit {
 
-    private final FormItemRepository itemRepository;
+    private final FormItemRepository fromItemRepository;
+    private final ItemRepository itemRepository;
 
     /**
      * 테스트용 데이터 추가
      */
     @PostConstruct
     public void init() {
-        itemRepository.save(new FormItem("itemA", 10000, 10));
-        itemRepository.save(new FormItem("itemB", 20000, 20));
+        fromItemRepository.save(new FormItem("itemA", 10000, 10));
+        fromItemRepository.save(new FormItem("itemB", 20000, 20));
+        itemRepository.save(new Item("itemA", 20000, 20));
+        itemRepository.save(new Item("itemB", 20000, 20));
     }
 
 }
